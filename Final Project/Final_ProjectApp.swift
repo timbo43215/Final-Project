949995f573159d1dd9639e8d78c339609c71547c
Tim Stack <tstack43215@yahoo.com>
2023-04-07 14:20:31 -0500

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by IIT PHYS 440 on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
